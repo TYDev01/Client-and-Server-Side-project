6add7c1de849ad081bbce24ff6db4c89f6ab0e75
TYDev01 <anthonychinaemeru84@yahoo.com>
2023-04-21 03:19:51 +0100

    Add my hero page and it's styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styles from '../styleAll.module.css'
+import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.hero}>
+        <div className={styles.heroText}>
+            <h2>The Best Way to <br/><span>Save</span>  &amp; Invest.</h2>
+            <p>Saved$ helps over 4 million customers achieve their financial<br />goals by helping them save and invest with ease.</p>
+            <Link to="/Signup">Create free Account</Link>
+        </div>
+        <div>
+            <p>motion</p>
+        </div>
+    </div>
   )
 }
